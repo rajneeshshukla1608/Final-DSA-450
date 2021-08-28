52587f576db3bb031d9acbe34200cc5e12975b2f
Rajneesh Shukla <65530868+rajneeshshukla1608@users.noreply.github.com>
2021-08-28 13:53:48 +0530

    reversed array

---
+++ Array/ArrayReverse.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+    int i,length,n;
+    printf("Enter the length of array: ");
+    scanf("%d", &n);
+    int arr[n];
+    length = sizeof(arr)/sizeof(arr[1]);
+    int end = length - 1;
+    printf("Enter the Number in to the array");
+    for (i = 0; i < length; i++)
+    {
+        scanf("%d", &arr[i]);
+    }    
+    
+    for(i = end; i >= 0; --i){
+        printf("%d", arr[i]);
+    }
+    return 0;
+}
