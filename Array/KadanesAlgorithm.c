67952f982cf2ee68fb997fa6706b938b991a061e
Rajneesh Shukla <65530868+rajneeshshukla1608@users.noreply.github.com>
2021-09-19 11:34:13 +0530

    Add files via upload

---
+++ Array/KadanesAlgorithm.c
@@ -0,0 +1,38 @@
+/*kadan's Algorithm for largest sum subarray problem*/
+#include<stdio.h>
+
+void LargestSumSubarray(int *arr, int n){
+    
+    int cursum = 0;
+    int maxsum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        cursum = cursum +  arr[i];
+        
+        if(cursum > maxsum){
+            maxsum = cursum;        
+            }
+        
+        if (cursum < 0){
+            
+            cursum = 0;
+
+        }
+    }
+    
+     printf(" The maximum sum of the subarray is: %d", maxsum);
+}
+
+int main(){
+    int n;
+    printf("Enter the size of an array: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter the elements in to the array: ");
+    for (int i = 0; i < n; i++){
+       scanf("%d", &arr[i]);
+    }
+    
+    LargestSumSubarray(arr, n);
+  
+}
