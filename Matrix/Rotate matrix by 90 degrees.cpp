56dd0219bbcd6ed81702efc8b0dbdf0ab6493a6e
Rajneesh Shukla <65530868+rajneeshshukla1608@users.noreply.github.com>
2021-12-21 19:45:28 +0530

    Create Rotate matrix by 90 degrees.cpp

---
+++ Matrix/Rotate matrix by 90 degrees.cpp
@@ -0,0 +1,11 @@
+void rotate(vector<vector<int>>& matrix) {
+        for(int i = 0; i<matrix.size(); i++){
+            for(int j = i; j < matrix[0].size(); j++){
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+      // reverse every row
+        for(int i = 0; i < matrix.size(); i++){
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
+    }
